1803431ad45de464b5e5483e926ca1f02f760873
Daniel Fernández <daniel89fg@gmail.com>
2023-09-28 11:13:27 +0200

    Tarea #2765 - Permite imprimir el nombre del terminal.

--- Lib/Tickets/BaseTicket.php
+++ Lib/Tickets/BaseTicket.php
@@ -413,6 +413,8 @@ abstract class BaseTicket
             }
         }
 
+        static::setHeaderTPV($model, $printer);
+
         // imprimimos el título del documento
         static::$escpos->text(static::sanitize($title) . "\n");
 
@@ -429,5 +431,16 @@ abstract class BaseTicket
             static::$escpos->text(static::sanitize($printer->head) . "\n\n");
             static::$escpos->setJustification();
         }
+    }
+
+    protected static function setHeaderTPV(ModelClass $model, TicketPrinter $printer)
+    {
+        if (false === Plugins::isEnabled('TPVneo') || false === $printer->print_name_terminal
+            || false === isset($model->idtpv) || empty($model->idtpv)) {
+            return;
+        }
+
+        $tpv = $model->getTerminal();
+        static::$escpos->text(static::sanitize(static::$i18n->trans('tpv') . ': ' . $tpv->name) . "\n");
     }
 }
